bd75e8ca432bc3a8c71176343ae296087a519dcd
Chuck <chuck.carr4@yahoo.com>
2022-03-09 01:27:00 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import RestaurantList from "./components/RestaurantList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Restaurant</h1>
+      <RestaurantList />
     </div>
+    
   );
 }
 
